ce120f264004ac35849a7dd3645a99ee9825e605
mdvitalie.ie <mdvitalie@gmail.com>
2023-02-02 17:24:34 +0000

    The script.sh has been modified and README fiel was added

---
+++ README.md
@@ -0,0 +1,9 @@
+# Week 2 Lab – Bash & Git
+
+# Vitalie x19241011
+
+# devops-lab-2-task
+# https://github.com/mdvitalie/devops-lab-2-task
+
+---
+To run the script.sh run: `bash script.sh`
--- script.sh
+++ script.sh
@@ -12,14 +12,27 @@ mkdir fol_2
 
 # Creating the three files in fol_1 and set their permissions
 touch fol_1/1_1.txt
+chmod 600 fol_1/1_1.txt
+
 touch fol_1/1_2.txt
+chmod 777 fol_1/1_2.txt
+
 touch fol_1/1_3.txt
+chmod 600 fol_1/1_3.txt
 
 # Creating the three files in fol_2 and set their permissions
 touch fol_2/2_1.txt
+chmod 600 fol_2/2_1.txt
+
 touch fol_2/2_2.txt
+chmod 777 fol_2/2_2.txt
+
 touch fol_2/2_3.txt
 chmod 600 fol_2/2_3.txt
 
 # Print a message indicating that the folders and files have been created
-echo "The folders and files have been created."
+echo "The folders and files have been created with the specified permissions."
+
+# Print a message indicating that the job has completed
+echo "Job completed."
+
